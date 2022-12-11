56f4838d58ac7a6937bd059d9b510c2c97efee32
Mr.Li <63112788+undercurre@users.noreply.github.com>
2022-12-12 02:04:51 +0800

    feat: ts加入、解析sheets

--- translate.ts
+++ translate.ts
@@ -1,1 +1,14 @@
-console.log("working");
+var XLSX = require('xlsx');
+
+var workbook = XLSX.readFile('./lib/17279000088888_test.xls').Workbook;
+
+var sheets = workbook.Sheets
+
+// 遍历sheet做处理
+
+sheets.forEach((element: any) => {
+    var aoa = XLSX.utils.sheet_to_json(element, {header: 1});
+});
+
+
+console.log(workbook)
